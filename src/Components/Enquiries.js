4c4df80cfb41a040ddea5d55e830aa00f5a43999
Raghupremshahapuram <100329614+Raghupremshahapuram@users.noreply.github.com>
2025-02-15 17:50:02 +0530

    added new

--- src/Components/Enquiries.js
+++ src/Components/Enquiries.js
@@ -14,38 +14,7 @@ const Enquiries = () => {
   if (status === "failed") return <p>Error: {error}</p>;
 
   return (
-    // <div>
-    //   <h2>User Enquiries</h2>
-    //   <ul>
-    //     {enquiries.map((enquiry, index) => (
-    //       <li key={index}>
-            
-    //         <strong>Name:</strong> {enquiry.name} | <strong>Email:</strong> {enquiry.email}
-    //       </li>
-    //     ))}
-    //   </ul>
-    // </div>
-  //   <div>
-  //   <h2>User Enquiries</h2>
-  //   <table border="1">
-  //     <thead>
-  //         <tr>
-  //         <th>Course Name</th>
-  //         <th>Name</th>
-  //         <th>Email</th>
-  //         </tr>
-  //     </thead>
-  //     <tbody>
-  //       {enquiries.map((enquiry, index) => (
-  //           <tr key={index}>
-  //           <td>{enquiry.course}</td> {/* ✅ Display Course Name */}
-  //           <td>{enquiry.name}</td>
-  //           <td>{enquiry.email}</td>
-  //           </tr>
-  //       ))}
-  //     </tbody>
-  //   </table>
-  // </div>
+ 
    <div>
    <h2>User Enquiries</h2>
    <table border="1">
